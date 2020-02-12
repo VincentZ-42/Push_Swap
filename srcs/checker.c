e306a3520c74841d427eb8b720b21dcd41501b83
Vincent  Zhao <vzhao@e1z3r8p14.42.us.org>
2020-02-12 11:36:29 -0800

    2.12.2020

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,37 +6,96 @@
 /*   By: vzhao <vzhao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 11:13:18 by vzhao             #+#    #+#             */
-/*   Updated: 2020/02/11 21:02:00 by vzhao            ###   ########.fr       */
+/*   Updated: 2020/02/12 11:35:55 by vzhao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include "../includes/dispatch_ps.h"
-#define CE(x) create_elem(x)
 
-// Right now, checker program just accepts commands and performs
-// operations on manually created stack a and b
+/*
+** Function that reads from standard input and applies operations to...
+** ...stack a or stack b based off existing operations in dispatch table
+** Args:
+** t_stack **a: pointer to head of linked list (stack a)
+** t_stack **b: pointer to head of linked list (stack b)
+** Returns:
+** Nothing, function manipulates pointers within stack a & b
+*/
+
+void	apply_operation(t_stack **a, t_stack **b)
+{
+	char	*operation;
+	int		i;
+
+	while (get_next_line(0, &operation) > 0)
+	{
+		i = -1;
+		while (++i < PS_OPERATIONS)
+			if (ft_strcmp(operation, PS_dispatch_table[i].type) == 0)
+				PS_dispatch_table[i].operation(a, b);
+		ft_strdel(&operation);
+	}
+	if (operation && *operation)
+		ft_strdel(&operation);
+}
+
+/*
+** Function that checks if our stack is sorted in ascending order
+** Args:
+** t_stack *a: linked list of integers (stack)
+** Returns:
+** 0 if stack is not sorted
+** 1 if stack is sorted
+*/
+
+int		is_sorted(t_stack *a)
+{
+	while (a && a->next)
+	{
+		if (a->n > a->next->n)
+			return (0);
+		a = a->next;
+	}
+	return (1);
+}
+
+/*
+** Main function that creates our stacks from argument input
+** Then applies operations to stack a & b by reading from standard input
+** Lastly, checks if stack a is sorted and stack b is empty to output...
+** ... either an OK for successful or KO for unsuccessful sort
+** Args:
+** int ac: argument count (# of argument inputs)
+** char **av: argument vectory (2D array of strings from program input)
+** Returns:
+** Nothing, program exits after either encountering an error or ...
+** ... or printing to standard output
+** NOTE: error checking occurs in create_stack function
+*/
 
 int     main(int ac, char **av)
 {
-    if (ac == 1)
-    {
-        printf("Usage: Enter Operation as Arg\n");
-        printf("(sa, sb, ss, pa, pb, ra, rb, rr, rra, rrb, or rrr)\n");
-        return (0);
-    }
-    t_stack *a = CE(2);
-    t_stack *b = CE(1);
-    b->next = CE(3);
-    b->next->next = CE(5);
-    a->next = CE(4);
-    a->next->next = CE(6);
+	t_stack *a;
+    t_stack *b;
+
+	if (ac == 1)
+		exit(1);
+	a = NULL;
+	b = NULL;
+	create_stack(&a, ac, av);
+	ft_printstack(&a);
+    ft_printstack(&b);
+	apply_operation(&a, &b);
+	// if (ac == 1)
+    // {
+    //     printf("Usage: Enter Operation as Arg\n");
+    //     printf("(sa, sb, ss, pa, pb, ra, rb, rr, rra, rrb, or rrr)\n");
+    //     return (0);
+    // }
+    ft_putstr("-----new stacks-----\n");
     ft_printstack(&a);
     ft_printstack(&b);
-    for (int i = 0; i < PS_OPERATIONS; i++)
-        if (ft_strcmp(av[1], PS_dispatch_table[i].type) == 0)
-            PS_dispatch_table[i].operation(&a, &b);
-    printf("new stacks\n");
-    ft_printstack(&a);
-    ft_printstack(&b);
+	ft_ps_exit((is_sorted(a) && !b ? "OK\n" : "KO\n"), standard_out);
+	// system("leaks checker");
 }
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: vzhao <vzhao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 14:48:32 by vzhao             #+#    #+#             */
-/*   Updated: 2020/02/11 21:29:18 by vzhao            ###   ########.fr       */
+/*   Updated: 2020/02/12 11:20:23 by vzhao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,30 @@ int     main(int ac, char **av)
 	a = NULL;
 	create_stack(&a, ac, av);
 	b = NULL;
-	ft_printstack(&a);
-	ft_printstack(&b);
+	// ft_printstack(&a);
+	// ft_printstack(&b);
+
+	// This is OK sequence of operations
+	// ft_putstr("rra\n");
+	// ft_putstr("pb\n");
+	// ft_putstr("sa\n");
+	// ft_putstr("rra\n");
+	// ft_putstr("pa\n");	
+
+	// This is KO sequence of operations
+	// ft_putstr("sa\n");
+	// ft_putstr("rra\n");
+	// ft_putstr("pb\n");
+
+	// Personal test
+	ft_putstr("sa\n");
+	ft_putstr("pb\n");
+	ft_putstr("pb\n");
+	ft_putstr("pb\n");
+	ft_putstr("sa\n");
+	ft_putstr("pa\n");
+	ft_putstr("pa\n");
+	ft_putstr("pa\n");		
+
 	// system("leaks push_swap");
 }
